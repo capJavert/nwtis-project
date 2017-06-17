dfff7d770b4057c3d5e00ec81cc0cd1314fa3142
capJavert <ante.baric3@gmail.com>
2017-06-17 16:05:38 +0200

    finished log filter

--- antbaric_aplikacija_2_2/src/java/org/foi/nwtis/antbaric/filters/LoggingFilter.java
+++ antbaric_aplikacija_2_2/src/java/org/foi/nwtis/antbaric/filters/LoggingFilter.java
@@ -1,7 +1,8 @@
 package org.foi.nwtis.antbaric.filters;
 
-
 import java.io.IOException;
+import java.util.Date;
+import javax.ejb.EJB;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -9,15 +10,37 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import org.foi.nwtis.antbaric.beans.Dnevnik;
+import org.foi.nwtis.antbaric.beans.DnevnikFacade;
+import org.foi.nwtis.antbaric.beans.UserAuth;
 
 @WebFilter("*.xhtml")
 public class LoggingFilter implements Filter {
 
+    @EJB
+    DnevnikFacade dnevnikFacade;
+    
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws ServletException, IOException {    
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        Long start = System.currentTimeMillis();
         System.out.println("FILTER WORKS!");
+
+        Dnevnik log = new Dnevnik();
+        log.setData(((HttpServletRequest) request).getRequestURL().toString());
+        log.setIpadresa(request.getRemoteAddr());
+        UserAuth userAuth = (UserAuth) ((HttpServletRequest) request).getSession().getAttribute("user");
+        if(userAuth != null) {
+            log.setKorisnik(userAuth.getUser().getUsername());
+        } else {
+            log.setKorisnik("PUBLIC");
+        }
+        log.setTrajanje((int) (System.currentTimeMillis()-start));
+        log.setVrijeme(new Date(System.currentTimeMillis()));
         
-        chain.doFilter(req, res);
+        dnevnikFacade.create(log);
+
+        chain.doFilter(request, response);
     }
 
     @Override
